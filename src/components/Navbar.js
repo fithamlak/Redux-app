056d9839ac6ac71d7dbd0827033a538a2687adda
Fithamlak <justiceofgod01@gmail.com>
2023-03-06 22:43:36 +0300

    Get data from store in to Navbar component using useSelector hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,17 @@
  import { useSelector } from 'react-redux';
 
  const Navbar = () => {
+    const { amount } = useSelector((store) => store.cart);
     return (
         <nav>
             <div nav-center>
                 <h3>Redux toolkit</h3>
+            </div>
+            <div nav-container>
+                <CartIcon />
+                <div className='amount-container'>
+                    <p className='total-amount'>{amount}</p>
+                </div>
             </div>
         </nav>
     )
