6913c6d1a5f420a1198b7510ef02ff764f209801
Fithamlak <justiceofgod01@gmail.com>
2023-03-06 22:33:47 +0300

    Create Navbar component and import is in add.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+ import { CartIcon} from '../icons';
+ import { useSelector } from 'react-redux';
+
+ const Navbar = () => {
+    return (
+        <nav>
+            <div nav-center>
+                <h3>Redux toolkit</h3>
+            </div>
+        </nav>
+    )
+ }
+
+ export default Navbar;
